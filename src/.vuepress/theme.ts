7e1a4df339d8147a3bf5a7a45cda93bfd3bbcdbf
我願一直向著陽光 <103393141+zgzyh@users.noreply.github.com>
2025-05-25 15:51:01 +0800

    自动为每个文件夹生成目录

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -178,6 +178,13 @@ export default hopeTheme({
     icon: {
       prefix: "fa6-solid:",
     },
+    
+    //自动为每个文件夹生成目录页面
+    plugins: {
+ 
+    autoCatalog: {
+      index: true
+    },
 
     // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
     // pwa: {
